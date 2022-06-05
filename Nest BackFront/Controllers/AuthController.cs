b480666c5f6cc4e953aec077b74c23f3d3bd77c8
416Shemi <tu7ngcsvr@code.edu.az>
2022-06-05 23:24:08 +0400

    new projeckt

---
+++ Nest BackFront/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Nest_BackFront.Models;
+using Nest_BackFront.ViewModels.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest_BackFront.Controllers
+{
+    public class AuthController : Controller
+    {
+        private UserManager<AppUser> _userManager { get; }
+        private SignInManager<AppUser> _signInManager { get; }
+        public AuthController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            AppUser newUser = new AppUser
+            {
+                Name = registerVM.Firstname,
+                Surname = registerVM.Lastname,
+                UserName = registerVM.Username,
+                Email = registerVM.Email
+            };
+            IdentityResult result = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            await _signInManager.SignInAsync(newUser, true);
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Register));
+        }
+    }
+}
